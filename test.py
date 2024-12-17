078d351e1b39204c2a6ab9fef99c54e75900b4b0
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2024-12-17 14:41:13 +0100

    ändra

--- test.py
+++ test.py
@@ -70,7 +70,32 @@ summa = int(x) + int(y)
 
 print (f'Summan är: {summa}')
 
+"""
+
+
+x = input('Välj sten, sax eller påse: ').lower()
+
+import random
+val = random.choice(['sten', 'sax', 'påse'])
+
+if x == val:
+    print(f"Det blev oavgjort! Både du och datorn valde {val}.")
+elif (x == 'sten' and val == 'sax') or (x == 'sax' and val == 'påse') or (x == 'påse' and val == 'sten'):
+    print(f"Grattis! Du valde {x} och datorn valde {val}. Du vann!")
+elif (x == 'sten' and val == 'påse') or (x == 'sax' and val == 'sten') or (x == 'påse' and val == 'sax'):
+    print(f"Tyvärr, du förlorade. Du valde {x} och datorn valde {val}.")
+else:
+    print("Ogiltigt val! Välj mellan 'sten', 'sax' eller 'påse'.")
 
 """
 
 
+text = "Data Science"
+
+print("Första tecknet:", text[0])
+
+print("Sista tecknet:", text[-1])
+
+print("De första fem tecknen:", text[:6])
+
+"""
