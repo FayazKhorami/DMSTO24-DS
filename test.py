0f3c907f84ccc3469ce6590d3a5712179b30e714
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2024-12-17 10:05:48 +0100

    ändra

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 
-print("Hello, World!")
+#print("Hello, World!")
 
-namn = "Anna"
+#namn = "Anna"
 
-age = 25
+#age = 25
 
-print(f"Hej, {namn}! Du är {age} år gammal.")
+#print(f"Hej, {namn}! Du är {age} år gammal.")
+
+x = 10
+
+y = 20 
+
+z = x + y
+
+print (f'Summan av x och y är: {z}')
