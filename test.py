2bc2f7cc6736df61a0d4dcb41f3ceefa0dd9c707
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2024-12-17 13:22:48 +0100

    ändra

--- test.py
+++ test.py
@@ -30,4 +30,36 @@ z = int(x)
 
 print (z * y)
 
+a = 7 + 3
+
+b = 5 // 2 
+
+c = 5 % 2 
+
+d = 5 ** 2
+ 
+print(a, b, c, d)
+
+x = 10
+
+y = 3
+
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+e = x // y
+f = x % y
+g = x ** y
+
+print (a , b, c, d, e, f, g)
+
 """
+
+pi = 3.14
+
+print (int(pi))
+
+y = 5
+
+print (float(y))
