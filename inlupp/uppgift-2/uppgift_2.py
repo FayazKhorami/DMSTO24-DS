82ab4a1bd0f73c97db75b0f7a00e01f8c51a422f
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2025-01-07 10:37:30 +0100

    ändra

--- inlupp/uppgift-2/uppgift_2.py
+++ inlupp/uppgift-2/uppgift_2.py
@@ -1,8 +1,11 @@
 # Uppgift 2
 # Skapa en funktion sum_list(numbers) som returnerar summan av alla siffror i listan.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
+
+def sum_list(numbers):
+    """Returnerar summan av alla siffror i listan."""
+    return sum(numbers)
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+print(sum_list(numbers))  # Output: 15
