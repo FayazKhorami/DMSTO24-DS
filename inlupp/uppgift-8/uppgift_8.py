4371b704dd7d3b0b8f41dd7053dad192875c7cd8
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2025-01-07 15:32:36 +0100

    uppgift8

--- inlupp/uppgift-8/uppgift_8.py
+++ inlupp/uppgift-8/uppgift_8.py
@@ -1,6 +1,6 @@
 # Uppgift 8
 # Skapa en funktion count_letters(string) som returnerar en dictionary med varje bokstav som nyckel och antalet förekomster som värde.
-"""
+
 
 def count_letters(string):
     letter_count = {}
@@ -15,16 +15,8 @@ def count_letters(string):
     
     return letter_count
 
-string = "hello world vad händer här jag du händer här"
-print(count_letters(string))  
-"""
 
 
-
-
-name = "Shubs"
-letters = {}
-for letter in name:
-    letters[letter] = letters.get(letter, 0)+1
-
-    letters
+print(count_letters("hello"))
+print(count_letters(""))
+print(count_letters("aaa"))
