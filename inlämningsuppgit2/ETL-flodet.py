2de7823156dcab0697637828310fd746c9188735
FayazKhorami <fayaz.ahmad.khorami.data24sto@edu.tucsweden.se>
2025-02-02 17:41:47 +0100

    lägga till alla

--- inlämningsuppgit2/ETL-flodet.py
+++ inlämningsuppgit2/ETL-flodet.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import matplotlib.pyplot as plt
 
 # Ändra visningsinställningar för att visa hela datasetet utan punkter
 pd.set_option("display.max_columns", None)  # Visa alla kolumner
